18e8d40c540b530071099fae984f58030ca93d44
Tatiana <tatiana.vakaruk1@gmail.com>
2025-06-17 20:41:46 +0300

    first commit

---
+++ fsfull2/jsfull/lesson14/task1/index.js
@@ -0,0 +1,11 @@
+let message = "Just learn it!";
+function sendMessage(name) {
+  const sender = "Gromcode";
+  console.log(`${name}, ${message} Your ${sender}`);
+}
+export function setMessage(text) {
+  message = text;
+}
+sendMessage("Tom");
+setMessage("Good job!");
+sendMessage("Tom");
