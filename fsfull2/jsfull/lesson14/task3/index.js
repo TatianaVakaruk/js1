916992a36d8bca2bc95a7c37fed32329308ee6f0
Tatiana <tatiana.vakaruk1@gmail.com>
2025-06-17 21:49:37 +0300

    first commit

---
+++ fsfull2/jsfull/lesson14/task3/index.js
@@ -0,0 +1,13 @@
+let number = 0;
+export function add(num) {
+  number += num;
+}
+export function decrease(num) {
+  number -= num;
+}
+export function reset() {
+  number = 0;
+}
+export function getMemo() {
+  return number;
+}
