aab8f271fca87c9f4643e07046bfb6617fd1939c
Tatiana <tatiana.vakaruk1@gmail.com>
2025-08-06 01:30:48 +0300

    Краткое описание изменений

--- fsfull2/jsfull/lesson29/task1/index.js
+++ fsfull2/jsfull/lesson29/task1/index.js
@@ -1,24 +1,24 @@
-export const addImg = (imgSrc, callback) => {
+const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement("img");
   imgElem.setAttribute("alt", "My photo");
   imgElem.src = imgSrc;
   const containerElem = document.querySelector(".page");
   containerElem.append(imgElem);
-  const onImageLoaded = () => {
+  const AddImage = () => {
     const { width, height } = imgElem;
     callback(null, { width, height });
   };
-  imgElem.addEventListener("load", onImageLoaded);
+  imgElem.addEventListener("load", AddImage);
   imgElem.addEventListener("error", () => callback("Image load is failed"));
 };
-const imgSrc = "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
-const onImageLoaded = (error, data) => {
+
+const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = data;
+  const { width, height } = imgElem;
   const sizeElem = document.querySelector(".image-size");
   sizeElem.textContent = `${width} x ${height}`;
 };
-addImg(imgSrc, onImageLoaded);
+addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
