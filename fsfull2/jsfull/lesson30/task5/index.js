fca69b2ae51977d47cf8d96db5a02d491d6dc44d
Tatiana <tatiana.vakaruk1@gmail.com>
2025-08-09 01:06:05 +0300

    Сообщение коммита

--- fsfull2/jsfull/lesson30/task5/index.js
+++ fsfull2/jsfull/lesson30/task5/index.js
@@ -20,7 +20,7 @@ export const addImageV2 = (url) => {
 });
 return p;
 };
-addImageV2('https://server.com/image.png')
+addImageV2('https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg')
   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
   .catch(error => console.log(error)); // ==> 'Image load failed'
 
