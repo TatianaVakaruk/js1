f7836ca8135047e9046063abab968e5ae59a0dba
Tatiana <tatiana.vakaruk1@gmail.com>
2025-08-16 01:48:47 +0300

    Сообщение коммита

--- fsfull2/jsfull/lesson32/task2/index.js
+++ fsfull2/jsfull/lesson32/task2/index.js
@@ -1,25 +1,26 @@
-const getRandomNumber = (from1, to)=>{
-  from1 + Math.random()*(to - from1);
-}
-const request = url => new Promise(resolve=>{
-  const randomDelay = getRandomNumber(1000, 3000);
-  setTimeout(()=>{
-    resolve({
-      userData:{
-        name:'Tom',
-        age:17,
-      },
-      source:url
-    });
-  }, randomDelay);
-})
+const getRandomNumber = (from1, to) => {
+  return from1 + Math.random() * (to - from1);
+};
+const request = (url) =>
+  new Promise((resolve) => {
+    const randomDelay = getRandomNumber(1000, 3000);
+    setTimeout(() => {
+      resolve({
+        userData: {
+          name: "Tom",
+          age: 17,
+        },
+        source: url,
+      });
+    }, randomDelay);
+  });
 const servers = [
-  'https://server.com/us/userId',
-  'https://server.com/eu/userId',
-  'https://server.com/au/userId'
+  "https://server.com/us/userId",
+  "https://server.com/eu/userId",
+  "https://server.com/au/userId",
 ];
-export const getUserASAP = userId =>{
-  const userUrls = servers.map(serverUrl=> `${serverUrl}/${userId}`);
-  const requests = userUrls.map(userUrl=>request(userUrl));
+export const getUserASAP = (userId) => {
+  const userUrls = servers.map((serverUrl) => `${serverUrl}/${userId}`);
+  const requests = userUrls.map((userUrl) => request(userUrl));
   return Promise.race(requests);
-}
+};
