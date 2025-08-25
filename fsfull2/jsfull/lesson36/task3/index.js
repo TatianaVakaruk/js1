2dffd0e27960fc4d46d5dd133e85938fa7c50259
Tatiana <tatiana.vakaruk1@gmail.com>
2025-08-26 01:31:24 +0300

    Add existing file

---
+++ fsfull2/jsfull/lesson36/task3/index.js
@@ -0,0 +1,21 @@
+export const getUsersBlogs = async users => {
+  try {
+    const usersData = users.map(user => {
+      const userData = fetch(`https://api.github.com/users/${user}`)
+        .then(response => {
+          return response.json();
+        })
+        .then(response => response.blog)
+        
+      return userData;
+    });
+    return await Promise.all(usersData);
+  } catch (err) {
+    throw new Error('Failed to load data');
+  }
+};
+
+// examples
+getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
+
